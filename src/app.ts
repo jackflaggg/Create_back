3ef327fb0cd4308cc7645663475c5fd7c7f9cc47
jackflaggg <rasul.khamzinnn@gmail.com>
2024-05-25 14:40:18 +0500

    add testingAllData

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, {Express, Request, Response} from "express";
 import {SETTINGS} from "./settings";
 import {videosRouter} from "./routes/videos-router";
+import {videos} from "./db";
+import {HTTP_STATUSES} from "./types";
 // import cors from 'cors';
 
 export const app: Express = express();
@@ -9,3 +11,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(SETTINGS.RouterPaths.videos, videosRouter);
 
+app.delete(SETTINGS.RouterPaths.testingAllData, (req: Request, res: Response) => {
+    videos.length = 0;
+    res.send(HTTP_STATUSES.NO_CONTENT_204)
+})
