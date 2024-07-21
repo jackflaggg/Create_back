7fccb61169d03daa97f207034b567e372301923e
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-21 19:26:20 +0500

    add db

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,10 @@ import {SETTINGS} from "./settings";
 import {videosRouter} from "./routes/videos-router";
 import {videos} from "./db/db";
 import {HTTP_STATUSES} from "./types/types";
-// import cors from 'cors';
 
 export const app: Express = express();
 
 app.use(express.json());
-// app.use(cors());
 app.use(SETTINGS.RouterPaths.videos, videosRouter);
 
 app.delete(SETTINGS.RouterPaths.testingAllData, (req: Request, res: Response) => {
